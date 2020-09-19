ef975ad860430567d9d3fea8ca13292e167316bc
John Leintz <johnleintz@scaledflow.com>
2020-09-19 15:20:47 -0500

    added sample code wrapping useState in a custom hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
+// this custom hook wraps useState
+// https://www.youtube.com/watch?v=6ThXsUwLWvc
+
 import React from 'react';
+import useLocalStorage from './components/useLocalStorage';
 
 function App() {
+  const [name, setName] = useLocalStorage('name', () => '')
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <input
+        type='text'
+        value={name}
+        onChange={e => setName(e.target.value)}
+        />
+    </>
   );
 }
 
 export default App;
+
+// import React, { useState } from 'react';
+
+// function App() {
+//   const [name, setName] = useState('')
+
+//   return (
+//     <>
+//       <input
+//         type='text'
+//         value={name}
+//         onChange={e => setName(e.target.value)}
+//         />
+//     </>
+//   );
+// }
+
+// export default App;
