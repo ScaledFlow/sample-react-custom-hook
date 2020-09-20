8d3fcb27465bf800781de8b2339ab41bf08bbb40
John Leintz <johnleintz@scaledflow.com>
2020-09-19 22:04:07 -0500

    add sample code

--- src/App.js
+++ src/App.js
@@ -3,27 +3,36 @@
 
 import React from 'react';
 import useLocalStorage from './components/useLocalStorage';
+import useUpdateLogger from './components/useUpdateLogger';
 
 function App() {
-  const [name, setName] = useLocalStorage('name', () => '')
-
+  console.log('App')
+  const [name, setName] = useLocalStorage('name', '')
+  useUpdateLogger(name)
+  
+  
   return (
+
     <>
       <input
         type='text'
         value={name}
         onChange={e => setName(e.target.value)}
         />
+        {console.log('app return')}
     </>
   );
 }
 
 export default App;
 
+//-------------------------------------------
 // import React, { useState } from 'react';
 
 // function App() {
 //   const [name, setName] = useState('')
+//   // console.log(useState);
+//   // console.log(setName);
 
 //   return (
 //     <>
--- src/components/useLocalStorage.js
+++ src/components/useLocalStorage.js
@@ -1,19 +1,25 @@
 import { useState, useEffect } from 'react'
 
 function getSaveValue(key, initialValue) {
+  console.log("getSaveValue")
   const savedValue = JSON.parse(localStorage.getItem(key))
-  if (savedValue) return savedValue
+  if (savedValue) 
+  return savedValue
 
-  if (initialValue instanceof Function) return initialValue()
+  if (initialValue instanceof Function) 
+  console.log("test")
   return initialValue
+
 }
 
 export default function useLocalStorage(key, initialValue) {
+  console.log("useLocalStorage")
   const [ value, setValue ] = useState(() => {
     return getSaveValue(key, initialValue)
   })
 
   useEffect(() => {
+    console.log("useEffect")
     localStorage.setItem(key, JSON.stringify(value))
   }, [value])
 
